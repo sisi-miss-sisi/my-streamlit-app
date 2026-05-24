9ce73f87e9c4ae9526cfa6e0154810ff342699c8
sisi-miss-sisi <19113699156@163.com>
2026-05-24 22:48:19 +0800

    Add files via upload

--- health_management.py
+++ health_management.py
@@ -1,11 +1,10 @@
-import streamlit as st # 做网页界面
-import pandas as pd # 用来存表格数据
+import streamlit as st  # 做网页界面
+import pandas as pd  # 用来存表格数据
 import pytz
-import datetime # 导入日期时间模块，记录日期和时间
-import plotly.express as px # 用来画折线图
-from io import BytesIO # 做PDF时临时存放数据用
-from supabase import create_client, Client # 导入云端数据库Supabase客户端，实现数据云同步
-
+import datetime  # 导入日期时间模块，记录日期和时间
+import plotly.express as px  # 用来画折线图
+from io import BytesIO  # 做PDF时临时存放数据用
+from supabase import create_client, Client  # 导入云端数据库Supabase客户端，实现数据云同步
 
 # 修改时间获取
 china_tz = pytz.timezone('Asia/Shanghai')
@@ -20,39 +19,6 @@ try:
 except Exception as e:
     st.error("⚠️ 未检测到数据库配置，请呼唤张茜博：在 Secrets 中设置 SUPABASE_URL 和 SUPABASE_KEY")
     st.stop()
-
-# 离线存储功能
-# session_state:Streamlit自带的“记忆储存”工具，用来在页面刷新时记住数据
-
-# 创建一个空的 离线血糖列表
-if 'offline_glucose' not in st.session_state:
-    st.session_state.offline_glucose = []
-# 创建一个空的 离线血压列表
-if 'offline_bp' not in st.session_state:
-    st.session_state.offline_bp = []
-
-# 定义保存数据的函数：有网络时存储到云端，无网络时存储到本地
-def save_record(table_name, data):
-    success = False
-
-    # 有网时，能够连上数据库
-    if supabase:
-        try:
-            supabase.table(table_name).insert(data).execute()
-            success = True
-        except Exception:
-            success = False
-    # 无网络时，数据缓存在session_state列表里面
-    if not success:
-        if table_name == "glucose":
-            st.session_state.offline_glucose.append(data)
-        else:
-            st.session_state.offline_bp.append(data)
-
-    return success
-
-
-
 
 # 页面美化
 st.set_page_config(page_title='老爸健康管家', layout='wide', initial_sidebar_state='collapsed')
@@ -71,8 +37,8 @@ st.markdown("""
     .stNumberInput input, .stSelectbox select {
         font-size: 1.2rem !important;
         }
-        
-        
+
+
         /*手机端优化*/
     @media only screen and (max-width: 600px) {
         .stButton>button {
@@ -92,13 +58,10 @@ st.markdown("""
             font-size: 12px !important;
             padding: 8px 4px;
         }
-        
+
     }
     </style>
     """, unsafe_allow_html=True)
-
-
-
 
 # 页面布局
 # 侧边筛选栏
@@ -108,7 +71,7 @@ range_option = st.sidebar.radio(
     "选择时间段",
     ["最近7天", "最近30天", "最近60天", "自定义"],
     index=1
- )
+)
 today = now_china.date()
 # 自定义筛选
 if range_option == "自定义":
@@ -125,53 +88,21 @@ else:
     end_date = today
     st.sidebar.write(f"当前显示：{start_date} 至 {end_date} 的记录")
 
-
-st.title("👨‍ 老爸健康数据管理系统") # st.title()大标题
-
-# 缓存数据自动同步
-pending_count = len(st.session_state.offline_glucose)+len(st.session_state.offline_bp)
-if pending_count!=0 and supabase:
-    try:
-        # supabase 往数据库里插入数据：supabase.table(表名).insert(数据).execute()
-        # 自动同步血糖
-        for i in st.session_state.offline_glucose[:]:
-            supabase.table("glucose").insert(i).execute()
-            st.session_state.offline_glucose.remove(i)
-
-        # 自动同步血压
-        for i in st.session_state.offline_bp[:]:
-            supabase.table("bp").insert(i).execute()
-            st.session_state.offline_bp.remove(i)
-
-        st.success("网络恢复，离线数据已自动同步到云端")
-        st.rerun() # 刷新页面
-
-    except:
-        pass
-
-
-# 缓存数据同步提示
-if pending_count !=0:
-   st.write(f"当前离线记录{pending_count}条，联网后将自动同步")
-else:
-    st.write("数据已加密存储于云端，手机与电脑实时同步") # st.caption 小字提示
-
+st.title("👨‍ 老爸健康数据管理系统")  # st.title()大标题
+st.write("数据已加密存储于云端，手机与电脑实时同步")  # st.caption 小字提示
 st.info("💡 点击左上角“ >> ”筛选日期范围")
-
-
 
 # 导航功能区：新建三个标签页
 tab1, tab2, tab3 = st.tabs(["📝 填写记录", "📂 数据管理", "📈 趋势分析"])
 
-
 # 第一部分：数据录入
-with tab1: # 把内容放在第一个标签页里面
+with tab1:  # 把内容放在第一个标签页里面
     option = st.radio("请选择测量项目：", ("血糖记录", "血压记录"), horizontal=True)
     # st.radio("提示文字", (选项1，选项2), horizontal=True)
     # horizontal=True:选项横着放；horizontal=False:选项竖着放
 
-    with st.form("input_form", clear_on_submit=True): # st.form("表单名", clear_on_submit=True):创建表单，提交后自动清空内容
-        if option == "血糖记录": # 选择了记录血糖之后出来的表单
+    with st.form("input_form", clear_on_submit=True):  # st.form("表单名", clear_on_submit=True):创建表单，提交后自动清空内容
+        if option == "血糖记录":  # 选择了记录血糖之后出来的表单
             d = st.date_input("日期", now_china.date())
             t = st.time_input("具体时间", now_china.time())
             options = [
@@ -190,17 +121,14 @@ with tab1: # 把内容放在第一个标签页里面
                 key="period_radio"
             )
 
-            v = st.number_input("血糖数值(mmol/L)", min_value = 0.0, max_value = 30.0, value = 10.0, step = 0.1)
-            n = st.text_input("备注","状态良好")
+            v = st.number_input("血糖数值(mmol/L)", min_value=0.0, max_value=30.0, value=10.0, step=0.1)
+            n = st.text_input("备注", "状态良好")
 
-            if st.form_submit_button("🚀 点击保存"): # 点击保存按钮后
+            if st.form_submit_button("🚀 点击保存"):  # 点击保存按钮后
                 try:
-                    data = {"日期": str(d), "具体时间": str(t)[:5], "测量时段":p, "血糖数值(mmol/L)":v, "备注":n}
-                    # supabase.table("glucose").insert(data).execute() # 把打包好的数据，存入云端数据库的 “glucose（血糖）表” 里
-                    if save_record("glucose", data):
-                        st.success("✅ 血糖数据已存入云库！")
-                    else:
-                        st.info("血糖数据已存入离线暂存区！")
+                    data = {"日期": str(d), "具体时间": str(t)[:5], "测量时段": p, "血糖数值(mmol/L)": v, "备注": n}
+                    supabase.table("glucose").insert(data).execute()  # 把打包好的数据，存入云端数据库的 “glucose（血糖）表” 里
+                    st.toast("✅ 血糖数据已存入云库！")
                 except Exception as e:
                     st.error(f"保存失败，请呼叫张茜博：{e}")
 
@@ -215,30 +143,29 @@ with tab1: # 把内容放在第一个标签页里面
 
             if st.form_submit_button("🚀 点击保存"):
                 try:
-                    data = {"日期": str(d), "具体时间": str(t)[0:5], "高压（收缩压）mmHg":sys, "低压（舒张压）mmHg":dia, "测量手臂":a, "心率":hr, "备注":note}
-                    if save_record("bp", data):
-                        st.success("✅ 血压数据已存入云库！")
-                    else:
-                        st.info("血压数据已存入离线暂存区！")
-                    # supabase.table("bp").insert(data).execute()
-                    # st.toast("✅ 血压数据已存入云库！")
+                    data = {"日期": str(d), "具体时间": str(t)[0:5], "高压（收缩压）mmHg": sys, "低压（舒张压）mmHg": dia, "测量手臂": a,
+                            "心率": hr, "备注": note}
+                    supabase.table("bp").insert(data).execute()
+                    st.toast("✅ 血压数据已存入云库！")
                 except Exception as e:
                     st.error(f"保存失败，请呼叫张茜博：{e}")
 
-
 # 第二部分：数据管理与导出
 with tab2:
-
     st.write(f"当前显示从{start_date} 至 {end_date} 的数据")
 
     # 获取血糖数据
-    res_g = supabase.table("glucose").select('*').gte("日期", str(start_date)).lte("日期", str(end_date)).order("日期", desc=True).order("具体时间",desc=True).execute() # desc=True:降序/desc=False:升序
+    res_g = supabase.table("glucose").select('*').gte("日期", str(start_date)).lte("日期", str(end_date)).order("日期",
+                                                                                                            desc=True).order(
+        "具体时间", desc=True).execute()  # desc=True:降序/desc=False:升序
     df_g = pd.DataFrame(res_g.data)
     if not df_g.empty:
         df_g['具体时间'] = df_g['具体时间'].astype(str).str[:5]
 
     # 获取血压数值
-    res_b = supabase.table("bp").select('*').gte("日期", str(start_date)).lte("日期", str(end_date)).order("日期", desc=True).order("具体时间",desc=True).execute() # desc=True:降序/desc=False:升序
+    res_b = supabase.table("bp").select('*').gte("日期", str(start_date)).lte("日期", str(end_date)).order("日期",
+                                                                                                       desc=True).order(
+        "具体时间", desc=True).execute()  # desc=True:降序/desc=False:升序
     df_b = pd.DataFrame(res_b.data)
     if not df_b.empty:
         df_b['具体时间'] = df_b['具体时间'].astype(str).str[:5]
@@ -257,7 +184,6 @@ with tab2:
             st.download_button("📥 下载血糖 Excel", output_g.getvalue(), "血糖记录.xlsx",
                                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
-
             st.dataframe(df_g, use_container_width=True, hide_index=True, height=400)
 
             # 删除功能
@@ -272,18 +198,16 @@ with tab2:
         else:
             st.write("暂无血糖记录")
 
-
-    #血压记录
+    # 血压记录
     with tab5:
 
         if not df_b.empty:
             # Excel导出功能
             output_b = BytesIO()
-            with pd.ExcelWriter(output_b,engine='xlsxwriter') as writer:
+            with pd.ExcelWriter(output_b, engine='xlsxwriter') as writer:
                 df_b.to_excel(writer, index=False, sheet_name='血压记录')
             st.download_button("📥 下载血压 Excel", output_b.getvalue(), "血压记录.xlsx",
                                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-
 
             st.dataframe(df_b, use_container_width=True, hide_index=True)
 
@@ -298,7 +222,6 @@ with tab2:
 
 # 第三部分：数据可视化（平均值计算）与打印
 with tab3:
-
     st.write(f"当前显示从{start_date} 至 {end_date} 的数据")
 
     tab6, tab7 = st.tabs(['血糖可视化', '血压可视化'])
@@ -306,14 +229,14 @@ with tab3:
     with tab6:
         if not df_g.empty:
 
-
             # 降采样或排序处理
             df_g_plot = df_g.sort_values("日期")
             df_g_plot["日期时间"] = df_g_plot["日期"] + " " + df_g_plot["具体时间"]
             df_g_plot["日期时间"] = pd.to_datetime(df_g_plot["日期时间"])
             df_g_plot = df_g_plot.sort_values("日期时间")
             # 绘图
-            fig_g_plot = px.line(df_g_plot, x="日期时间", y="血糖数值(mmol/L)", hover_data=["测量时段"], markers=True, title="血糖长期趋势图")
+            fig_g_plot = px.line(df_g_plot, x="日期时间", y="血糖数值(mmol/L)", hover_data=["测量时段"], markers=True,
+                                 title="血糖长期趋势图")
             # 优化横坐标
             fig_g_plot.update_layout(xaxis=dict(tickangle=-45, tickformat='%y-%m-%d %H:%M'))
             # 显示图表
@@ -333,12 +256,8 @@ with tab3:
         else:
             st.write("暂时还没有录入血糖数据哦~")
 
-
-
     with tab7:
         if not df_b.empty:
-
-
 
             # 降采样或排序处理
             df_b_plot = df_b.sort_values("日期")
@@ -351,7 +270,6 @@ with tab3:
             fig_b_plot.update_layout(xaxis=dict(tickangle=-45, tickformat='%Y-%m-%d %H:%M'))
             st.plotly_chart(fig_b_plot, use_container_width=True)
             st.info("💡 提示：将鼠标悬停在图表右上角，点击‘相机’图标可下载高清打印图片")
-
 
             # 计算平均值
             # 高压
